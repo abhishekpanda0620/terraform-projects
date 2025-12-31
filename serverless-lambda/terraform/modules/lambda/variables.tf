f8dd39a0bd385a3b4956a1548c0c3023b2550232
Abhishek Panda <abhishek.panda0620@gmail.com>
2025-12-31 15:16:07 +0530

    adding lambda image processor serverless

---
+++ serverless-lambda/terraform/modules/lambda/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+    type = string
+}
+
+variable "lambda_function_name" {
+    type = string
+}
+
+variable "upload_bucket_arn" {
+    type = string
+}
+
+variable "processed_bucket_id" {
+    type = string
+}
+
+variable "processed_bucket_arn" {
+    type = string
+}
+
+variable "aws_region" {
+    type = string
+}
---
+++ serverless-lambda/terraform/modules/s3/variables.tf
@@ -0,0 +1,9 @@
+variable "upload_bucket_name" {
+    description = "Name of the upload bucket"
+    type        = string
+}
+
+variable "processed_bucket_name" {
+    description = "Name of the processed bucket"
+    type        = string
+}
